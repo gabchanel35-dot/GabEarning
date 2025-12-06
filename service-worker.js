5802e16a8586bd2ce28354888892f016f3bebcaa
Blockraiders <gabchanel35@gmail.com>
2025-12-06 14:53:19 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const cacheName = "gab-earnings-cache-v1";
+const filesToCache = ["/index.html","/manifest.json","/icon.png"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache))); });
+self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
